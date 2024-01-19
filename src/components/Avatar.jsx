5dbdb5c08df5049f905b2430958f1c1534133408
Will Cline <willrcline.atx@gmail.com>
2024-01-19 15:49:31 -0600

    converting

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,7 +1,6 @@
 import { useState, useRef, useEffect, useContext, createContext } from "react";
 import AvatarVideo from "./AvatarVideo";
 import ChatBox from "./ChatBox";
-import { StartSession, StopSession } from "./AvatarVideoControls.jsx";
 
 export const AvatarContext = createContext(null);
 
@@ -9,39 +8,11 @@ export const Avatar = () => {
     const [avatarSynthesizer, setAvatarSynthesizer] = useState(null);
     const myAvatarVideoEleRef = useRef();
     const myAvatarAudioEleRef = useRef();
-    const [sessionStarted, setSessionStarted] = useState(false);
-
-
-    const avatarLoaded = useRef(false);
-
-
-    // useEffect(() => {
-
-    //     // const start = async () => {
-    //     //     await startSession();
-    //     // }
-    //     console.log("AvatarVideo.jsx useEffect___")
-    //     const delay = setTimeout(() => {
-    //         if (avatarLoaded.current === false) {
-    //             avatarLoaded.current = true;
-    //         console.log("delay ended___")
-    //         StartSession();
-    //         // start()
-    //     }
-    //     }, 5000);
-        
-    //     return () => {
-    //         console.log("Cleaning up: Component unmounting or effect re-running");
-    //         clearTimeout(delay);
-    //     };
-    //   }, []);
 
     return(
         <div className="container" style={styles.myAvatarContainer}>
             <div style={{ display: 'flex', flexDirection: "column", alignItems: 'center' }}>
                 <AvatarContext.Provider value={{avatarSynthesizer, setAvatarSynthesizer, myAvatarAudioEleRef, myAvatarVideoEleRef}}>
-                    <StartSession />
-                    <StopSession />
                     <AvatarVideo />
                     <ChatBox />
                 </AvatarContext.Provider>
